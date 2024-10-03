1270625fe5b04855f6e7122cef6e21faa65e032b
theritiksuthar <63701883+ritiksuthar@users.noreply.github.com>
2024-10-03 21:25:48 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+<script>
+// JavaScript for interactivity
+document.addEventListener("DOMContentLoaded", function() {
+    const connectButton = document.querySelector('.button');
+    connectButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        alert('Thanks for connecting with Ritik! He will reach out to you soon.');
+    });
+});
+</script>
